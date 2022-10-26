96760d336ee78b78972351d11afe2c1d654c2407
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-10-26 12:44:53 +0300

    run migration for apps

--- hokelaprjct/settings.py
+++ hokelaprjct/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "hokelaapp",
+    "crispy_forms",
+    "rest_framework",
+    "django_registration",
+    "bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -73,10 +78,13 @@ WSGI_APPLICATION = "hokelaprjct.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hokelaproject',
+        'USER': 'andrew',
+        'PASSWORD': 'hokela12345',
     }
 }
 
