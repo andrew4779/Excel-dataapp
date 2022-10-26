a92976015b35e3fce965e7c028c4a31a10a072d6
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-10-26 10:50:53 +0300

    make new changes

---
+++ hokelaapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.excel_upload, name='excel_upload'),
+    path('register/',views.register,name ='register'),
+    path("login", views.login_user, name="login"),
+    path('logout/', views.logout_user, name='logout'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
