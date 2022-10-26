cfc7d7ef9e79d62973ef1c36f01d6d180fc29b05
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-10-26 11:18:50 +0300

    Add bootsrap

--- hokelaapp/forms.py
+++ hokelaapp/forms.py
@@ -1,18 +1,18 @@
-from django import forms
-from .models import *
-from django.forms import ModelForm
-from django.contrib.auth import login,authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django import forms
+# from .models import *
+# from django.forms import ModelForm
+# from django.contrib.auth import login,authenticate
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+# class RegisterForm(UserCreationForm):
+#     email = forms.EmailField()
 
-    class Meta:
-        model = User
-        fields = ['username','email','password1', 'password2',]
+#     class Meta:
+#         model = User
+#         fields = ['username','email','password1', 'password2',]
 
-class LoginForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ["username", "password"]
+# class LoginForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ["username", "password"]
--- hokelaapp/urls.py
+++ hokelaapp/urls.py
@@ -4,10 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.excel_upload, name='excel_upload'),
-    path('register/',views.register,name ='register'),
-    path("login", views.login_user, name="login"),
-    path('logout/', views.logout_user, name='logout'),
+    # path('',views.excel_upload, name='excel_upload'),
+    path('', views.home, name = 'home'),
+    # path('register/',views.register,name ='register'),
+    # path("login", views.login_user, name="login"),
+    # path('logout/', views.logout_user, name='logout'),
 
 ]
 
